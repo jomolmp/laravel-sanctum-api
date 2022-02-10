f4ed53b9a740a1e7e95d91caa4ca9924418008db
jomol <jomolmp1998@gmail.com>
2022-02-10 13:11:36 +0530

    table name specified in product and task controllers

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+    protected $table = 'products';
     protected $fillable = [
         'name',
         'description',
