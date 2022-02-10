944ad37d7a09f9345d8982e5ced032e114a59c77
jomol <jomolmp1998@gmail.com>
2022-02-10 13:17:43 +0530

    table name specified in task controller changed to tasks

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
-    protected $table = 'products';
+    protected $table = 'tasks';
     protected $fillable = [
         'name',
         'description',
